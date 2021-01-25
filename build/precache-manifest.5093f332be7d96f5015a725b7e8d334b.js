da08d24eb8c31238b1df44a81ae4a7ad8fbbe124
Ishdeep Singh <ishdeep.711@gmail.com>
2021-01-25 17:11:25 -0500

    adding changes to favicon

---
+++ build/precache-manifest.5093f332be7d96f5015a725b7e8d334b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7070bcc0353581b1c62213ecf70f1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b15c60c655e0943134d",
+    "url": "/static/js/2.05b18d6a.chunk.js"
+  },
+  {
+    "revision": "8da9ff4ecc7440c09f64cd8625501c41",
+    "url": "/static/js/2.05b18d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ba2b04262c8b70b9eb",
+    "url": "/static/js/main.be58e539.chunk.js"
+  },
+  {
+    "revision": "b4d12b70b7d90d24ec54",
+    "url": "/static/js/runtime-main.9496a8dc.js"
+  }
+]);
